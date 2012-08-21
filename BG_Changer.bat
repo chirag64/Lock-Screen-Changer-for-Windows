72c1dc49c9144876822cca4c99d0127bc879c0e7
Chirag <chiroo64@gmail.com>
2012-08-22 01:38:31 +0530

    Assigns image.jpg as bg image
    Fixed the problem so that user can Run as Admin

--- BG_Changer.bat
+++ BG_Changer.bat
@@ -1,10 +1,12 @@
 @ECHO OFF
 COLOR 0A
-IF NOT (%1)==() GOTO LABEL2
+CD /D %~dp0
+IF EXIST image.jpg GOTO LABEL2
 :LABEL1
+ECHO %CD%
 ECHO Welcome to Chiroo's Logon background image changing script (Windows 7)
 ECHO.
-ECHO To use this script, drag an image on the file name instead of opening it by double-click.
+ECHO To use this script, keep a jpg image in the same folder as this and name it as "image.jpg". You will then need to right-click this program and select "Run as Administrator" for it to work correctly
 ECHO.
 ECHO The image must be in jpg format and must be smaller than 245KiB, and must have one of the following resolutions...
 ECHO.
@@ -18,12 +20,8 @@ GOTO END
 :LABEL2
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\Background" /V OEMBackground /T REG_DWORD /d 00000001 /f
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /V OEMBackground /T REG_DWORD /d 00000001 /f
-CD /D C:\WINDOWS\SYSTEM32\
-MD oobe
-CD oobe
-MD info
-CD info
-MD backgrounds
-CD backgrounds
-COPY /Y %1 backgroundDefault.jpg || ECHO Some Error Occurred && PAUSE
+MD C:\WINDOWS\SYSTEM32\oobe
+MD C:\WINDOWS\SYSTEM32\oobe\info
+MD C:\WINDOWS\SYSTEM32\oobe\info\backgrounds
+COPY /Y image.jpg "C:\WINDOWS\SYSTEM32\oobe\info\backgrounds\backgroundDefault.jpg" || ECHO Some Error Occurred && PAUSE
 :END
